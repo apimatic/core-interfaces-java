b9ce2291b6ac2144003c08d3679fd323f1f9f107
Hamza Shoukat <50887243+hamzashoukat94@users.noreply.github.com>
2022-10-13 14:42:04 +0500

    fix(add-square-customized-response-handling): add context initializer (#18)
    A functional interface is being added to support the square customized response handling.
    
    closes #17

---
+++ src/main/java/io/apimatic/coreinterfaces/type/functional/ContextInitializer.java
@@ -0,0 +1,21 @@
+package io.apimatic.coreinterfaces.type.functional;
+
+import java.io.IOException;
+import io.apimatic.coreinterfaces.http.Context;
+
+
+/**
+ * Functional Interface to apply the context initialization function for the response models.
+ *
+ * @param <ResponseType> The type of the response in which context initialised.
+ */
+@FunctionalInterface
+public interface ContextInitializer<ResponseType> {
+   /**
+    * 
+    * @param context the context of the api call
+    * @param response the response from the server
+    * @return the Response
+    */
+   ResponseType apply(Context context, ResponseType response);
+}
