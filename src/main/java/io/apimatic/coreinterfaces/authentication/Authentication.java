45de1a4f34471180b0140d4a5deeb29551a599b2
Muhammad Sufyan <sufyankhanrao123@gmail.com>
2023-10-03 12:01:39 +0500

    feat(multiple-auth): add support for multiple auth
    This commit updates the authentication interface and incorporates the instance methods used for the validation of auth schemes. other than validation results, it also stores the error message for each auth scheme instance.
    
    closes #50

--- src/main/java/io/apimatic/coreinterfaces/authentication/Authentication.java
+++ src/main/java/io/apimatic/coreinterfaces/authentication/Authentication.java
@@ -5,17 +5,55 @@ import io.apimatic.coreinterfaces.http.request.Request;
 /**
  * To setup methods for authentication
  */
-public interface Authentication {
+public abstract class Authentication {
 
+    
+    private boolean isValid;
+    
+    private String errorMessage;
+    
     /**
      * Apply the authentication on the httpRequest
      * @param httpRequest the request on which authentication is being applied
      * @return the authenticated request
      */
-    Request apply(Request httpRequest);
+    public abstract Request apply(Request httpRequest);
 
     /**
      * Validates the auth params for the httpRequest
      */
-    void validate();
+    public abstract void validate();
+    
+    /**
+     * Checks if the auth credentials are valid. 
+     * @return true if the auth credentials are valid.
+     */
+    public boolean isValid() {
+        return isValid;
+    }
+    
+    /**
+     * Sets the validatity of the auth credentials.
+     * @param isValid the flag to set for validity.
+     */
+    public void setValidity(boolean isValid) {
+        this.isValid = isValid;
+    }
+    
+    /**
+     * Returns the error message if the auth credentials are not valid. 
+     * @return The string message whenever the auth credentials are not valid.
+     */
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+    
+    /**
+     * Sets the error message for invalid auth credentials.
+     * @param errorMessage the error message to set.
+     */
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+    
 }
