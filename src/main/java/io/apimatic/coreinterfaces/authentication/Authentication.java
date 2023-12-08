7599199be02b81c5f86b686c810f60b390656379
Muhammad Sufyan <muhammad.sufyan@apimatic.io>
2023-12-08 11:34:01 +0500

    Revert "feat(multiple-auth): add support for multiple auth (#51)"
    This reverts commit f16afbe7abc25614707ba90afe09fcfc7f8bdb20.

--- src/main/java/io/apimatic/coreinterfaces/authentication/Authentication.java
+++ src/main/java/io/apimatic/coreinterfaces/authentication/Authentication.java
@@ -5,59 +5,17 @@ import io.apimatic.coreinterfaces.http.request.Request;
 /**
  * To setup methods for authentication
  */
-public abstract class Authentication {
+public interface Authentication {
 
     /**
-     * Stores the validity of the auth scheme.
+     * Apply the authentication on the httpRequest
+     * @param httpRequest the request on which authentication is being applied
+     * @return the authenticated request
      */
-    private boolean isValid;
+    Request apply(Request httpRequest);
 
     /**
-     * Stores the error message for the auth scheme.
+     * Validates the auth params for the httpRequest
      */
-    private String errorMessage;
-
-    /**
-     * Applies the authentication on the httpRequest.
-     * @param httpRequest the request on which authentication is being applied.
-     * @return the authenticated request.
-     */
-    public abstract Request apply(Request httpRequest);
-
-    /**
-     * Validates the credentials for authentication.
-     */
-    public abstract void validate();
-
-    /**
-     * Checks if the auth credentials are valid.
-     * @return true if the auth credentials are valid, false otherwise.
-     */
-    public boolean isValid() {
-        return isValid;
-    }
-
-    /**
-     * Sets the validatity of the auth credentials.
-     * @param isValid the flag to set for validity.
-     */
-    public void setValidity(boolean isValid) {
-        this.isValid = isValid;
-    }
-
-    /**
-     * Returns the error message if the auth credentials are not valid.
-     * @return the string message whenever the auth credentials are not valid.
-     */
-    public String getErrorMessage() {
-        return errorMessage;
-    }
-
-    /**
-     * Sets the error message for invalid auth credentials.
-     * @param errorMessage the error message to set.
-     */
-    public void setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage;
-    }
+    void validate();
 }
