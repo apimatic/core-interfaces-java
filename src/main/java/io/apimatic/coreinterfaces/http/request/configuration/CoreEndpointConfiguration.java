013b74487f8bbb8f8d9fe28a15db0eb7f95face3
hamzamahmood <hamzamehmood18@gmail.com>
2024-04-18 11:43:18 +0500

    remove array serialization format from endpoint configuration

--- src/main/java/io/apimatic/coreinterfaces/http/request/configuration/CoreEndpointConfiguration.java
+++ src/main/java/io/apimatic/coreinterfaces/http/request/configuration/CoreEndpointConfiguration.java
@@ -1,7 +1,5 @@
 package io.apimatic.coreinterfaces.http.request.configuration;
 
-import io.apimatic.coreinterfaces.http.request.ArraySerializationFormat;
-
 /**
  * The configuration for an endpoint.
  */
@@ -18,10 +16,4 @@ public interface CoreEndpointConfiguration {
      * @return the boolean which tells endpoint has binary response or not
      */
     boolean hasBinaryResponse();
-
-    /**
-     * Enumeration for all ArraySerialization formats
-     * @return the array serialization format
-     */
-    ArraySerializationFormat getArraySerializationFormat();
 }
